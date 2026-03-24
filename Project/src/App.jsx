f9d03bdfb66f969617d40021c5b28f1f1ccfd728
Omkar Ghadge <omkarghadge68@gmail.com>
2026-03-24 12:21:15 +0530

    apis added

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { fetchPhotos, fetchVideos } from './api/mediaAPI'
 
 const App = () => {
   return (
     <div className='h-screen w-full bg-zinc-950 text-[#fafafa]'
-    >App</div>
+    >
+      
+      <button className='bg-green-400 px-4 py-2 rounded mr-4' onClick={async () => {
+        const data = await fetchPhotos('dog')
+        console.log(data.results);
+      }}>Fetch Photos</button>
+      <button className='bg-blue-400 px-4 py-2 rounded' onClick={async () => {
+        const data = await fetchVideos('dog')
+        console.log(data.videos);
+      }}>Fetch Videos</button>
+    </div>
   )
 }
 
